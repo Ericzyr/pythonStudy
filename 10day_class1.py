7041a6071aabb86031d3590006615fc6ddc16ed4
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-03-26 11:13:57 +0800

    pc7

--- 10day_class1.py
+++ 10day_class1.py
@@ -3,17 +3,47 @@
 class person:
 
     comput = 'This is my desk' #属于类时的，也叫静态字段
-    def __init__(self,name,age,add):
+
+
+    # 公共的性的属性
+    def __init__(self, name, age, add):
+
         #动态字段
         self.Name = name
         self.Age = age
         self.Add = add
 
-p1=person('jack',25,'shandong')   #属于对像 也叫
+
+
+
+    # 功能（特有属性）
+    def go_school(self,city):
+        # print("%s go to school is the %s" % (self.Name,city))  #% 格式化字符串
+        print("{} go to school is the {}".format(self.Name, city))   #% format 格式化字符串 两种方式
+        self.city = city
+
+
+    def go_work(self,where):
+        print('{} work is the {}'.format(self.Name,where))
+
+
+
+
+
+
+p1=person('jack',25,'shandong')   #属于对像 也叫 实例化对象
+
+
+print(p1.city)
+
 p2=person('alex',56,'beijing')
 
+# print(p1.Name,p1.Add,p1.Age)
+
+p1.go_school("beijing")
+
+p2.go_work("shanghai")
 
 
 
 
-print(p1.Name,p1.Add,p1.Age)
