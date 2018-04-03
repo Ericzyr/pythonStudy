0c79948474396813a86da92a5d4305d2fb5b570b
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-04-03 18:23:31 +0800

    pc7

--- module_re.py
+++ module_re.py
@@ -8,11 +8,40 @@ import re
 str = 'This is my1 desk'
 s = re.findall('^T.*k$', str)#搜索查找
 
-""" 
+''' 
 
 ^表示以什么开始　$表示以什么结束　.表示任意一个字符　*表示多个字符　+表示１至多个字符
 
-"""
+
+
+re.match与re.search的区别
+
+re.match只匹配字符串的开始，如果字符串开始不符合正则表达式，则匹配失败，函数返回None；而re.search匹配整个字符串，直到找到一个匹配。
+
+
+
+
+compile(编译、编制) 函数
+
+compile 函数用于编译正则表达式，生成一个正则表达式（ Pattern ）对象，供 match() 和 search() 这两个函数使用。
+
+语法格式为：
+re.compile(pattern[, flags])
+
+
+
+
+findall（全文查找）
+
+在字符串中找到正则表达式所匹配的所有子串，并返回一个列表，如果没有找到匹配的，则返回空列表。
+
+注意： match 和 search 是匹配一次 findall 匹配所有。
+
+语法格式为：
+
+findall(string[, pos[, endpos]])
+
+'''
 
 print(s)
 
@@ -40,17 +69,14 @@ print("电脑的IP地址：",get[0],'\n')
 def f1():
     df = os.popen('df -h').read()
     print(df,'\n')
-
     df_file = re.compile('/dev/sda(.*)%').findall(df)
     print(df_file)
 
-
     s = df_file[0]
+
     q=s.replace(" ","")
 
-    r = re.sub('  ','',df_file[0])
-    z = re.sub(' ','\n',r)
-    l = list()
-    print(q[-2:])
+
+    print('查看电脑存贮量剩余：',q[-2:])
 f1()
 
--- web27.py
+++ web27.py
@@ -1,7 +1,18 @@
 #!/usr/bin/evn python
 # -*-coding:utf-8-*-
+import os
+import re
 
 
-name,age = 'hello',25
-print(name)
-print(age)
+ip = input("please input TV's IP address:")
+ip_add = os.popen('adb connect '+ip).read()
+ip_devices = os.popen('adb devices').read()
+
+if  ip_add[0:7] == 'already':
+    print('connect sucess')
+elif ip_add[0:6] == 'unable':
+    print('unconnect faile')
+
+
+print(ip_add[0:7])
+print(ip_devices)
